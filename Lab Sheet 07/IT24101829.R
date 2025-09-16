cf8e5cd093f92fe7029c3f0a5024dadbbd67b51e
it24101829 <it24101829@my.sliit.lk>
2025-09-16 23:18:04 +0530

    IT24101829_Lab_07

---
+++ Lab Sheet 07/IT24101829.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\LENOVO\\OneDrive\\Desktop\\IT24101829")
+getwd()
+
+#Q1
+punif(10, min=0, max=40, lower.tail=FALSE)-punif(25, min=0, max=40, lower.tail=FALSE)
+
+
+#Q2
+pexp(0, rate=1/3, lower.tail=FALSE)-pexp(2, rate=1/3, lower.tail=FALSE)
+
+#Q3.1
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+
+#Q3.2
+qnorm(0.05, mean=100, sd=15, lower.tail=FALSE)
